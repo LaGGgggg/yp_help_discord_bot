73af58c8701d3a8428c82ab66d9b0a981692e8d8
Nikita <Poni22poni23@yandex.ru>
2024-01-04 14:38:25 +0400

    feat: структура изменена на cog-образную
    Теперь файлы, которые находятся в модуле, задаваемом переменной окружения COGS_DIR_NAME загружаются как cog-и (не .py и начинающиеся с "_" файлы игнорируются).
    Создавать новые cog классы рекомендуется наследованием от базового CogBase класса в cogs/_cog_base.py.
    В каждом cog файле должна (must) находится асинхронная функция get_cog, возвращающая класс, наследуемый от discord.ext.commands.Cog.
    Весь ранее созданный функционал бота был перенесён в cogs/question_cog.py.
    Добавлена переменная окружения COGS_DIR_NAME.

---
+++ cogs/_cog_base.py
@@ -0,0 +1,19 @@
+from typing import Callable
+from logging import Logger
+
+from discord.ext.commands import Cog
+
+from settings import Settings
+
+
+class CogBase(Cog):
+
+    def __init__(self, bot, get_logger_function: Callable[[str], Logger], bot_settings: Settings) -> None:
+
+        self.bot = bot
+        self.logger = get_logger_function(__name__)
+        self.bot_settings = bot_settings
+
+    @Cog.listener()
+    async def on_ready(self) -> None:
+        self.logger.info(f'Cog logged on as {self.bot.user}')
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from discord import Intents, Message
-from discord.ext.commands import Bot, context
+from importlib import import_module
+from os import listdir
+
+from discord import Intents
+from discord.ext.commands import Bot
 from tortoise import Tortoise
 
 from logging_config import get_logger
 from settings import get_settings, Settings
-from views import QuestionThemeMenuView
-from models import get_question_by_discord_channel_id
 
 
 class CustomBot(Bot):
@@ -13,6 +14,7 @@ class CustomBot(Bot):
     def __init__(self, bot_settings: Settings, *args, **kwargs) -> None:
 
         self.bot_settings = bot_settings
+        self.logger = get_logger(__name__)
 
         super().__init__(*args, **kwargs)
 
@@ -22,12 +24,35 @@ class CustomBot(Bot):
 
         await Tortoise.generate_schemas()
 
+        for file_name in listdir(self.bot_settings.COGS_DIR_NAME):
+
+            # skip all non-python files and supporting files (starts with "_")
+            if '.py' not in file_name or file_name[0] == '_':
+
+                self.logger.info(f'Non-cog file found, skip it: "{file_name}"')
+
+                continue
+
+            self.logger.info(f'Start cog file adding: "{file_name}"')
+
+            try:
+
+                cog_module = import_module(f"{self.bot_settings.COGS_DIR_NAME}.{file_name.rsplit('.py', 1)[0]}")
+
+                cog_class = await cog_module.get_cog()
+
+                await self.add_cog(cog_class(self, get_logger, self.bot_settings))
+
+            except Exception as e:
+                self.logger.error(f'An error occurred while cog adding: {e}')
+
+            else:
+                self.logger.info(f'Cog file adding completed successfully')
+
 
 def main() -> None:
 
     # Preparation:
-
-    logger = get_logger(__name__)
 
     bot_settings = get_settings()
 
@@ -40,48 +65,6 @@ def main() -> None:
     intents.message_content = True
 
     bot = CustomBot(bot_settings=bot_settings, command_prefix='/', intents=intents)
-
-    @bot.event
-    async def on_ready() -> None:
-        logger.info(f'Logged on as {bot.user}')
-
-    @bot.event
-    async def on_message(message: Message) -> None:
-
-        # don't respond to ourselves
-        if message.author == bot.user:
-            return
-
-        await bot.process_commands(message)
-
-    @bot.command()
-    async def new_question(ctx: context.Context) -> None:
-        await ctx.send('Выберите тип вопроса', view=QuestionThemeMenuView(bot, bot_settings))
-
-    @bot.command()
-    async def complete_current_question(ctx: context.Context) -> None:
-
-        question = await get_question_by_discord_channel_id(ctx.channel.id)
-
-        if not question:
-
-            await ctx.send('Вопрос не найден, убедитесь, что пишите эту команду в ветке с вопросом')
-
-            return
-
-        if question.is_completed:
-
-            await ctx.send('Вопрос уже помечен как завершённый')
-
-            return
-
-        question.is_completed = True
-
-        await question.save(update_fields=('is_completed',))
-
-        await ctx.channel.edit(name=f'[РЕШЕНО] {ctx.channel.name}')
-
-        await ctx.send('Вопрос помечен как решённый')
 
     bot.run(bot_settings.BOT_TOKEN)
 
